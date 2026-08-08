402887759f5057be51b5aef407413e8d85a6fc2f
webmaster442 <ruzsinszki.gabor@gmail.com>
2026-08-08 21:19:05 +0200

    Add HtmlCssClassesTokenRenderer for HTML syntax output
    Introduced a sealed HtmlCssClassesTokenRenderer class implementing ITokenRenderer. This class generates HTML with CSS classes for syntax-highlighted tokens, ensures HTML encoding, and wraps output in <pre> and <code> tags. Includes a ToString() method for retrieving the final HTML.

---
+++ src/CodePunk.Highlight.Core/SyntaxHighlighting/HtmlCssClassesTokenRenderer.cs
@@ -0,0 +1,76 @@
+﻿using CodePunk.Highlight.Core.SyntaxHighlighting.Abstractions;
+using CodePunk.Highlight.Core.SyntaxHighlighting.Tokenization;
+using System.Text;
+using System.Web;
+
+namespace CodePunk.Highlight.Core.SyntaxHighlighting;
+
+/// <summary>
+/// A token renderer that generates HTML with CSS classes for syntax highlighting.
+/// </summary>
+public sealed class HtmlCssClassesTokenRenderer : ITokenRenderer
+{
+    private readonly StringBuilder _html;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HtmlCssClassesTokenRenderer"/> class.
+    /// </summary>
+    public HtmlCssClassesTokenRenderer()
+    {
+        _html = new StringBuilder();
+    }
+
+    /// <inheritdoc/>
+    public void BeginRender()
+    {
+        _html.AppendLine("<pre class=\"code-block\">");
+        _html.AppendLine("<code>");
+    }
+
+    /// <inheritdoc/>
+    public void EndRender()
+    {
+        _html.AppendLine("</code>");
+        _html.AppendLine("</pre>");
+    }
+
+    /// <inheritdoc/>
+    public void RenderToken(Token token)
+    {
+        var classToEmit = GetClass(token.Type);
+        var htmlTokenValue = HttpUtility.HtmlEncode(token.Value);
+        if (!string.IsNullOrEmpty(classToEmit))
+        {
+            _html.Append($"<span class=\"{classToEmit}\">{htmlTokenValue}</span>");
+        }
+        else
+        {
+            _html.Append(htmlTokenValue);
+        }
+    }
+
+    private static string GetClass(TokenType type)
+    {
+        return type switch
+        {
+            TokenType.Text => "text",
+            TokenType.Keyword => "keyword",
+            TokenType.Type => "type",
+            TokenType.String => "string",
+            TokenType.Comment => "comment",
+            TokenType.Number => "number",
+            TokenType.Operator => "operator",
+            TokenType.Punctuation => "punctuation",
+            TokenType.Identifier => "identifier",
+            TokenType.Preprocessor => "preprocessor",
+            _ => string.Empty,
+        };
+    }
+
+    /// <summary>
+    /// Returns the rendered HTML as a string.
+    /// </summary>
+    /// <returns>The rendered HTML.</returns>
+    public override string ToString()
+        => _html.ToString();
+}
